9274bbb2ca000dbb64636048d538662473367b5e
shakilattar <59828272+shakilattar@users.noreply.github.com>
2020-01-15 19:47:55 +0530

    Create sortfunc2.py

---
+++ sortfunc2.py
@@ -0,0 +1,9 @@
+def sortfunc():
+    numbers = [4, 2, 16, 9, 25, 64, 81, 100]
+    print(numbers)
+    # sort values in descending numbers
+    numbers.sort(reverse=True)
+    print(numbers)
+
+
+sortfunc()
